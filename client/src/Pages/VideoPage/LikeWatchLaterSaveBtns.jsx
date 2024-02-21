03b2884026be8a6d2db4ed6ca3ad419b06da11de
adarsh716 <alakhanpal2003@gmail.com>
2024-02-21 23:22:56 +0530

    first commit

--- client/src/Pages/VideoPage/LikeWatchLaterSaveBtns.jsx
+++ client/src/Pages/VideoPage/LikeWatchLaterSaveBtns.jsx
@@ -1,106 +1,133 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 import { BsThreeDots } from "react-icons/bs";
+
 import {
-    AiFillDislike,
-    AiFillLike,
-    AiOutlineDislike,
-    AiOutlineLike,
-  } from "react-icons/ai";
-  
-  import { MdPlaylistAddCheck } from "react-icons/md";
-  import {
-    RiHeartAddFill,
-    RiPlayListAddFill,
-    RiShareForwardLine,
-  } from "react-icons/ri";
-  import "./LikeWatchLaterSaveBtns.css";
-function LikeWatchLaterSaveBtns() {
-    const [SAveVideo, setSAveVideo] = useState(false);
-    const [DislikeBtn, setDislikeBtn] = useState(false);
+  AiFillDislike,
+  AiFillLike,
+  AiOutlineDislike,
+  AiOutlineLike,
+} from "react-icons/ai";
+
+import { MdPlaylistAddCheck } from "react-icons/md";
+import {
+  RiHeartAddFill,
+  RiPlayListAddFill,
+  RiShareForwardLine,
+} from "react-icons/ri";
+import "./LikeWatchLaterSaveBtns.css";
+import { useDispatch, useSelector } from "react-redux";
+import { likeVideo } from "../../actions/video";
+import { addTolikedVideo, deletelikedVideo } from "../../actions/likedVideo";
+import { useEffect } from "react";
+import { addTowatchLater, deleteWatchLater } from "../../actions/watchLater";
+
+function LikeWatchLaterSaveBtns({ vv, vid }) {
+  const CurrentUser = useSelector((state) => state?.currentUserReducer);
+  const dispatch = useDispatch();
+  const [SAveVideo, setSAveVideo] = useState(false);
+  const [DislikeBtn, setDislikeBtn] = useState(false);
   const [LikeBtn, setLikeBtn] = useState(false);
 
+  const likedVideoList = useSelector((state) => state.likedVideoReducer);
+  
+  const watchLaterList= useSelector(state=>state.watchLaterReducer)
+
+  useEffect(() => {
+    likedVideoList?.data
+      .filter(
+        (q) => q?.videoId === vid && q?.Viewer === CurrentUser?.result._id
+      )
+      .map((m) => setLikeBtn(true));
+    watchLaterList?.data
+      .filter(
+        (q) => q?.videoId === vid && q?.Viewer === CurrentUser?.result._id
+      )
+      .map((m) => setSAveVideo(true));
+  }, []);
+
   const toggleSavedVideo = () => {
-    //if (CurrentUser) {
+    if (CurrentUser) {
       if (SAveVideo) {
         setSAveVideo(false);
-        // dispatch(
-        //   deleteWatchLater({
-        //     videoId: vid,
-        //     Viewer: CurrentUser?.result._id,
-        //   })
-        // );
+        dispatch(
+          deleteWatchLater({
+            videoId: vid,
+            Viewer: CurrentUser?.result._id,
+          })
+        );
       } else {
         setSAveVideo(true);
-        // dispatch(
-        //   addTowatchLater({
-        //     videoId: vid,
-        //     Viewer: CurrentUser?.result._id,
-        //   })
-        // );
+        dispatch(
+          addTowatchLater({
+            videoId: vid,
+            Viewer: CurrentUser?.result._id,
+          })
+        );
       }
-    // } else {
-    //   alert("Plz Login To save the video !");
-    // }
+    } else {
+      alert("Plz Login To save the video !");
+    }
   };
 
   const toggleLikeBtn = (e, lk) => {
-    //if (CurrentUser) {
+    if (CurrentUser) {
       if (LikeBtn) {
         setLikeBtn(false);
-        // dispatch(
-        //   likeVideo({
-        //     id: vid,
-        //     Like: lk - 1,
-        //   })
-        // );
-        // dispatch(deletelikedVideo({
-        //   videoId: vid,
-        //   Viewer: CurrentUser?.result._id,
-        // }))
+        dispatch(
+          likeVideo({
+            id: vid,
+            Like: lk - 1,
+          })
+        );
+        dispatch(deletelikedVideo({
+          videoId: vid,
+          Viewer: CurrentUser?.result._id,
+        }))
       } else {
         setLikeBtn(true);
-        // // dispatch(
-        // //   likeVideo({
-        // //     id: vid,
-        // //     Like: lk + 1,
-        // //   })
-        // );
-        // dispatch(
-        //   addTolikedVideo({
-        //     videoId: vid,
-        //     Viewer: CurrentUser?.result._id,
-        //   })
-        // );
-        // setDislikeBtn(false);
+        dispatch(
+          likeVideo({
+            id: vid,
+            Like: lk + 1,
+          })
+        );
+        dispatch(
+          addTolikedVideo({
+            videoId: vid,
+            Viewer: CurrentUser?.result._id,
+          })
+        );
+        setDislikeBtn(false);
       }
-    // } else {
-    //   alert("Plz Login To give a like");
-    // }
+    } else {
+      alert("Plz Login To give a like");
+    }
   };
 
   const toggleDislikeBtn = (e, lk) => {
-    //if (CurrentUser) {
+    if (CurrentUser) {
       if (DislikeBtn) {
         setDislikeBtn(false);
       } else {
-       setDislikeBtn(true);
-    //     if (LikeBtn) {
-    //       dispatch(
-    //         likeVideo({
-    //           id: vid,
-    //           Like: lk - 1,
-    //         })
-    //       );
-    //       dispatch(deletelikedVideo({
-    //         videoId: vid,
-    //         Viewer: CurrentUser?.result._id,
-    //       }))
-    //     }
-    //     setLikeBtn(false);
+        setDislikeBtn(true);
+        if (LikeBtn) {
+          dispatch(
+            likeVideo({
+              id: vid,
+              Like: lk - 1,
+            })
+          );
+          dispatch(deletelikedVideo({
+            videoId: vid,
+            Viewer: CurrentUser?.result._id,
+          }))
+        }
+        setLikeBtn(false);
       }
-    // } else {
-    //   alert("Plz Login To give a like");
-    // }
+    } else {
+      alert("Plz Login To give a like");
+    }
   };
   return (
     <div className="btns_cont_videoPage">
@@ -111,27 +138,22 @@ function LikeWatchLaterSaveBtns() {
       <div className="btn_VideoPage">
         <div
           className="like_videoPage"
-          //onClick={(e) => toggleLikeBtn(e,vv.Like)}
-          onClick={() => toggleLikeBtn()}
+          onClick={(e) => toggleLikeBtn(e, vv.Like)}
         >
           {LikeBtn ? (
             <>
               <AiFillLike size={22} className="btns_videoPage" />
             </>
           ) : (
-            <> 
+            <>
               <AiOutlineLike size={22} className="btns_videoPage" />
             </>
           )}
-          <b>
-            1k
-            {/* {vv.Like} */}
-            </b>
+          <b>{vv.Like}</b>
         </div>
         <div
           className="like_videoPage"
-         // onClick={(e) => toggleDislikeBtn(e, vv.Like)}
-         onClick={() => toggleDislikeBtn()}
+          onClick={(e) => toggleDislikeBtn(e, vv.Like)}
         >
           {DislikeBtn ? (
             <>
@@ -144,9 +166,7 @@ function LikeWatchLaterSaveBtns() {
           )}
           <b>DISLIKE</b>
         </div>
-        <div className="like_videoPage"
-         onClick={() => toggleSavedVideo()}
-         >
+        <div className="like_videoPage" onClick={() => toggleSavedVideo()}>
           {SAveVideo ? (
             <>
               <MdPlaylistAddCheck size={22} className="btns_videoPage" />
